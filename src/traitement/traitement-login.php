059038fb9f6e0cdf3cad25ead62fbfff6fdd7c09
LOPES_Hu <hugo.lop2304@gmail.com>
2022-09-19 12:26:20 +0200

    traitement login et register

--- src/traitement/traitement-login.php
+++ src/traitement/traitement-login.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once '../bdd/Database.php';
+
+$bdd = new Database();
+
+$log = $bdd->getBdd()->prepare("SELECT * FROM Utilisateur");
+$log->execute();
+
+//verifier
+//en attente
--- src/traitement/traitement-register.php
+++ src/traitement/traitement-register.php
@@ -1,1 +1,31 @@
 <?php
+
+require_once '../bdd/Database.php';
+
+$bdd = new Database();
+
+try {
+    $reg = $bdd->getBdd()->prepare("SELECT * FROM Utilisateur");
+    $reg->execute();
+
+    if(!$res = $reg->fetch()){
+        $inscrit = $bdd->getBdd()->prepare("INSERT INTO Utilisateur (nom,prenom,email,mdp,admin,actif) VALUES (?,?,?,?,?,?)");
+        $inscrit->execute(array(
+            'nom'=>$_POST['nom'],
+            'prenom'=>$_POST['prenom'],
+            'email'=>$_POST['email'],
+            'mdp'=>$_POST['mdp'],
+            'admin'=>0,
+            'actif'=>0
+        ));
+
+        header('Location: ../../vue/login.php');
+    }else{
+        header('Location: ../../vue/register.php');
+    }
+
+}catch(PDOException $e){
+    echo $e;
+}
+
+
