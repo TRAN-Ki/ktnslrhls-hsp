73620c3db5b912299cc4dc47ef81d7f573d74dfb
TRAN_Ki <arknxpro@gmail.com>
2022-09-19 09:38:24 +0200

    fix: ajout hydrate sur la class Utilisateur.php

--- src/modele/Utilisateur.php
+++ src/modele/Utilisateur.php
@@ -2,5 +2,27 @@
 
 class Utilisateur
 {
+    private $id_utilisateur;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $mdp;
+    private $admin;
+    private $actif;
 
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+
+            if (method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
 }
