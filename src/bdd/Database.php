54ab68f98fd8c5c9ba5dc41b10f7925cc5a13e2b
LOPES_Hu <hugo.lop2304@gmail.com>
2022-09-19 11:39:28 +0200

    update database

---
+++ src/bdd/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+
+    private $bdd;
+    private $host;
+    private $dbname;
+    private $user;
+    private $pwd;
+
+    public function __construct()
+    {
+        $this->host = "localhost";
+        $this->dbname = "hsp-skh";
+        $this->user = "root";
+        $this->pwd = "";
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getBdd()
+    {
+        return $this->bdd = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->pwd);
+    }
+
+
+
+
+}
