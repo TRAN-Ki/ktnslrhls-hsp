11e19e65cb5114c996dc4582dc385e6eacf05a16
VoltanVI <h.lopes@lprs.fr>
2022-10-14 23:51:25 +0200

    add: back validation inscriptions + role admin

--- vue/panelAdmin/homeAdmin.php
+++ vue/panelAdmin/homeAdmin.php
@@ -5,6 +5,23 @@ if($_SESSION['admin'] == 0){
     header("Location: ../login.php");
 }
 else{
+
+    // Menu Actif
+
+    // <select user>
+    $user = new Utilisateur(array(
+            'admin' => $_POST['admin'],
+            'actif' => $_POST['actif']
+
+    ));
+    // Pour attribué à un utilisateur le role admin
+    if($_POST['admin'] = 'Administrateur') {
+        $user->setAdmin(1);
+    }
+    // Pour validé un utilisateur qui a créé un compte (validé inscriptions)
+    if($_POST['actif'] = 'Vérifié') {
+        $user->setActif(1);
+    }
   ?>
 
 
